d8d219f00c5e697efce0867dc90b7d0b5c7a53d1
haileyhansard <haileyhansard@gmail.com>
2020-10-02 13:23:57 -0600

    Cleaned up code for ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,62 +1,47 @@
-#UPER
-#INPUT: weights in a list, the limit of weights, and the length, all as integers
-    # weights = [4, 6, 10, 15, 16]
-#OUTPUT: a TUPLE of integers of the 2 INDEXES of the WEIGHTS in the list. 
+# UPER
+# INPUT: weights (integers) in a list, the length of list, and the limit of weights (the sum of two weights)
+    # weights = [4, 6, 10, 15, 16], 5, 21
+# OUTPUT: a TUPLE of integers of the 2 INDEXES of the WEIGHTS in the list that sum to the limit.
     # Higher value should be at index 0 of output list, smaller at index 1
     # (3, 1)
     # (greater_weight_index, smaller_weight_index)
 
-#Store each weight in list as KEYS
-# Each weight's list index is its value
-#Trying to find the two numbers in the list whose weights sum up to the limit
-# LIMIT is the number that the two "packages" should add up to.
+# Store each weight in list as KEYS
+# Each weight's list index is its VALUE
+# Trying to find the two numbers in the list whose weights sum up to the limit
+# LIMIT is the number that the two "weights" should add up to.
 
 
-def get_indices_of_item_weights(weights, length, limit): #not working, come back to
+def get_indices_of_item_weights(weights, length, limit): 
     """
     YOUR CODE HERE
     """
     # Your code here
     cache = {}
 
-    # index = 0
-
     #keys are the weights
     #values are the indexes
-
-    # for num in weights:
-    #     if num in cache:
-    #         cache[num, 'dupe'] = index
-    #     else:
-    #         cache[num] = index
-    #     index += 1
-
-    #     return cache
-
-    # for key in cache:
-    #     if (limit - key) in weights and (limit - key) == key:
-    #         greater_index = cache[key, 'dupe']
-    #         smaller_index = cache[key]
-    #         return [greater_index, smaller_index]
-    #     if (limit - key) in weights:
-    #         smaller_index = cache[key]
-    #         greater_index = cache[limit - key]
-    #         return [greater_index, smaller_index]
+    # {weight: index0, weight: index1, weight: index2}
 
     #enumuerate loops through the index and the elements in the 'weights' list
+    #enumerate is perfect because I'm keeping track of the index and the element value
     for index, first_weight in enumerate(weights):
         #the total weight limit minus the first_weight gives us the second_weight
-        second_weight = limit - first_weight
         print(index, first_weight)
-        #print(second_weight)
+        #assign the value of the limit - first_weight to 'second_weight' so we know what number we are looking for
+        second_weight = limit - first_weight
+        print(second_weight)
 
-        #want to add the second_weight into the cache until we find the second_weight that is the target! that adds up to the limit
+        #the first_weight and its index will be added to cache, and each time it iterates, it will subtract limit - first_weight and return the answer, which is the second_weight, our target for the next item we want to add to our tuple.
+        #once if finds the second_weight target in the cache, it will return the first index, then the second_weights' index
         if second_weight in cache:
             return (index, cache[second_weight])
         cache[first_weight] = index
         #print(index)
-        #print(cache)
+        print(cache)
+        print("Next iteration")
 
+    #it will print in order of the higher index because it will already have the second_weight in the cache, so it will print its index first which will be FURTHER down the array, meaning HIGHER INDEX. Then when its done looping through the first_weights to find the missing weight that adds to sum, it's index will be entered in next in the tuple
     return None
 
 
